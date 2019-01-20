b2bbb3d6714e77eba019dc0bd460c3a662cc5540
makeitokay <andrey.mu11@yandex.ru>
2019-01-20 20:08:11 +0500

    Создан главный файл (для непосредственного запуска игры)

---
+++ okayjet.py
@@ -0,0 +1,12 @@
+from game import Game
+from start_screen import Start
+from settings import *
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+start = Start(FPS, screen)
+start.run()
+game = Game(FPS, screen)
+game.run()
