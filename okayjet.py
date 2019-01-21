dddd00da97e295b50c51fb8933a55e9cdf7a224c
Protocs <utki.nikita@yandex.ru>
2019-01-21 21:00:30 +0500

    Подправил константы

--- okayjet.py
+++ okayjet.py
@@ -5,8 +5,8 @@ from okayjet.start_screen import Start
 from okayjet.settings import GAME_NAME, SCREEN_SIZE, FPS
 
 pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.flip()
+screen = pygame.display.set_mode(SCREEN_SIZE)
+pygame.display.set_caption(GAME_NAME)
 
 start = Start(FPS, screen)
 start.run()
