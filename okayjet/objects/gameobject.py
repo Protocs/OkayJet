5a6a89a0ea600e283df06bac165c56cf10022624
Protocs <utki.nikita@yandex.ru>
2019-01-23 16:34:52 +0500

    Add files via upload

--- okayjet/objects/gameobject.py
+++ okayjet/objects/gameobject.py
@@ -1,6 +1,21 @@
-import pygame
+from pygame.sprite import Sprite
+
+from ..util import load_image
 
 
-class GameObject(pygame.sprite.Sprite):
-    def __init__(self, group):
-        super().__init__(group)
+class GameObject(Sprite):
+    SPRITE_GROUPS = []
+
+    def __init__(self, game, x, y):
+        super().__init__(*(game.sprite_groups[g] for g in ('all', *self.SPRITE_GROUPS)))
+
+        self.game = game
+
+        self.image = load_image(self.IMAGE)
+
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+
+    def move(self, dx, dy):
+        self.rect = self.rect.move(dx, dy)
--- okayjet/objects/player.py
+++ okayjet/objects/player.py
@@ -1,24 +1,23 @@
 import pygame
+
 from .gameobject import GameObject
-from okayjet import util
 from ..settings import SCREEN_WIDTH
+from .. import game as game_module
+
 
 class Player(GameObject):
-    def __init__(self, group, x, y):
-        super().__init__(group)
-        self.image = util.load_image("player.png")
-        self.rect = self.image.get_rect()
-        self.rect.x = x
-        self.rect.y = y
-        self.speedup = 0.1
+    IMAGE = "player.png"
 
-    def move(self, dx, dy):
-        self.rect = self.rect.move(dx, dy)
+    def __init__(self, game, x, y):
+        super().__init__(game, x, y)
+        self.speedup = 0.1
 
     def update(self):
         if self.rect.x < SCREEN_WIDTH // 4:
             self.rect = self.rect.move(3, 0)
+
         space = pygame.key.get_pressed()[pygame.K_SPACE]
+
         if self.rect.y < 355 and not space:
             self.rect.y += (3 * self.speedup)
             self.speedup += 0.05
