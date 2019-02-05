7ad7376c0b14ff2225865ec611a1e1c253a9d65c
nikitakosatka <nikitakosatka@yandex.ru>
2019-02-05 11:22:06 +0500

    Добавлен звук начала игры

--- okayjet/start_screen.py
+++ okayjet/start_screen.py
@@ -18,6 +18,7 @@ class Start:
         self.background = load_image("start_background.png")
         self.start_button = load_image("press_somewhere_image.png")
         self.logo = load_image("logo.png")
+        self.start_sound = pygame.mixer.Sound("data/music/play_sound.wav")
 
         pygame.mixer.music.load("data/music/start.mp3")
         pygame.mixer.music.play(-1)
@@ -30,11 +31,14 @@ class Start:
     def events(self):
         for event in pygame.event.get():
             if event.type in (pygame.MOUSEBUTTONUP, pygame.KEYUP):
+                self.start_sound.play()
                 self.start = False
+
             elif event.type == pygame.QUIT:
                 terminate()
 
     def update(self):
+        self.surface.fill((0, 0, 0))
         self.surface.blit(self.background, (0, 0))
         self.background.blit(self.start_button, (150, 400))
         self.background.blit(self.logo, (280, 75))
